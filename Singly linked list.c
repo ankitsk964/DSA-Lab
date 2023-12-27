b6b7d3f507049a92d3ac095a625190b7788420a2
Ankit Kumar <gamingwithankit964@gmail.com>
2023-12-27 15:34:45 +0530

    Added comments

--- Singly linked list.c
+++ Singly linked list.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct singly{
+typedef struct singly{ // Created structure of node
 	int data;
 	struct singly *next;
 }sll;
-sll *start, *new, *link;
+sll *start, *new, *link; // Start is used to store address of starting node of linked list
+						 // new is used to store newly created node address
+						 // link is used to link 2 nodes
 
 void create();
 void count();
@@ -15,7 +17,7 @@ void delete();
 void main(){
 	start = NULL;
 	int c;
-	do{
+	do{ // To display the menu to user infinite times
 		printf("\n 1. Create Linked list");
 		printf("\n 2. Display Linked list");
 		printf("\n 3. Count nodes in linked list");
@@ -48,20 +50,20 @@ void main(){
 		}
 	}while(1);
 }
-void create(){
+void create(){ // To create new nodes
 	new = NULL;
 	char choice = 'y';
 	while(choice == 'y' || choice == 'Y'){
 		new = (sll*)malloc(sizeof(sll));
-		if(new == NULL){
+		if(new == NULL){ // To check if the new node is created or not
 			printf("\n Memory not allocated");
 			exit(1);
 		}
 		printf("\n Node created : ");
 		printf("\n Enter data :- ");
-		scanf("%d", &new->data);
+		scanf("%d", &new->data); // To enter data in newly created node
 		new->next = NULL;
-		if(start == NULL){
+		if(start == NULL){ // To check if the node is first node or not
 			start = new;
 			link = new;
 		}
@@ -69,12 +71,13 @@ void create(){
 			link->next = new;
 			link = new;
 		}
+		// To get  choice of user if he/she wants to create more nodes or not
 		printf("\n Do you want to create More nodes ?");
 		printf("\n Press 'Y' for Yes and 'N' for No : ");
 		scanf("%d", &choice);
 	}
 }
-void display(){
+void display(){ // Display function to display Linked list
 	
 	link = start;
 	while(link != NULL){
@@ -82,7 +85,7 @@ void display(){
 		link = link->next;
 	}
 }
-void count(){
+void count(){ // To count number of nodes in linked list
 	int count = 0;
 	link = start;
 	while(link != NULL){
@@ -91,26 +94,26 @@ void count(){
 	}
 	printf("\n Total nodes are :- %d", count);
 }
-void insert(){
+void insert(){ // To insert a new node in linked list
 	int count = 1;
 	sll *c = start;
 	sll *temp;
 	int n;
 	new = (sll*)malloc(sizeof(sll));
-	if(new == NULL){
+	if(new == NULL){ // To check if new node is created or not
 		printf("\n Memory not allocated");
 		exit(1);
 	}
-	printf("\n Enter data :- ");
+	printf("\n Enter data :- "); // To enter data in newly created node
 	scanf("%d", &new->data);
 	new->next = NULL;
 	printf("\n Enter the position to insert :- ");
 	scanf("%d", &n);
-	if(n == 1){
+	if(n == 1){ // To enter node at begining
 		new->next = start;
 		start = new; 
 	}
-	else{
+	else{ // To enter node in between 2 nodes
 		while(count != (n-1) && temp != NULL){
 			count++;
 			c = c -> next;
@@ -124,7 +127,7 @@ void insert(){
 	}
 	printf("\n Node Inserted at position  : %d ", n);
 }
-void delete(){
+void delete(){ // To delete a node from linked list
 	int count = 1;
 	sll *c = start;
 	sll *temp;
@@ -132,12 +135,12 @@ void delete(){
 	
 	printf("\n Enter the number of node to be deleted :- ");
 	scanf("%d", &n);
-	if(n==1){
+	if(n==1){ // To delete a node from begining 
 		temp = start;
 		start = start->next;
 		free(temp);
 	}
-	else{
+	else{ 	// To delete a node  from between of 2 nodes
 		while(count != (n-1)){
 			count++;
 			c = c -> next;
